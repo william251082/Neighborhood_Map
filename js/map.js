cf0944f37a77355975b3fc8505b63be7a69db7ea
William del Rosario <williamdelrosario@yahoo.com>
2017-08-04 13:30:08 +0200

    Added Commute Distance Matrix API

--- js/map.js
+++ js/map.js
@@ -168,6 +168,9 @@ var map;
         document.getElementById('zoom-to-area').addEventListener('click', function() {
           zoomToArea();
         });
+        document.getElementById('search-within-time').addEventListener('click', function() {
+          searchWithinTime();
+        });
 
         // Add an event listener so that the polygon is captured,  call the
         // searchWithinPolygon function. This will show the markers in the polygon,
@@ -319,5 +322,90 @@ var map;
                     ' specific place.');
               }
             });
+        }
+      }
+
+           // This function allows the user to input a desired travel time, in
+      // minutes, and a travel mode, and a location - and only show the listings
+      // that are within that travel time (via that travel mode) of the location
+      function searchWithinTime() {
+        // Initialize the distance matrix service.
+        var distanceMatrixService = new google.maps.DistanceMatrixService;
+        var address = document.getElementById('search-within-time-text').value;
+        // Check to make sure the place entered isn't blank.
+        if (address == '') {
+          window.alert('You must enter an address.');
+        } else {
+          hideListings();
+          // Use the distance matrix service to calculate the duration of the
+          // routes between all our markers, and the destination address entered
+          // by the user. Then put all the origins into an origin matrix.
+          var origins = [];
+          for (var i = 0; i < markers.length; i++) {
+            origins[i] = markers[i].position;
+          }
+          var destination = address;
+          var mode = document.getElementById('mode').value;
+          // Now that both the origins and destination are defined, get all the
+          // info for the distances between them.
+          distanceMatrixService.getDistanceMatrix({
+            origins: origins,
+            destinations: [destination],
+            travelMode: google.maps.TravelMode[mode],
+            unitSystem: google.maps.UnitSystem.IMPERIAL,
+          }, function(response, status) {
+            if (status !== google.maps.DistanceMatrixStatus.OK) {
+              window.alert('Error was: ' + status);
+            } else {
+              displayMarkersWithinTime(response);
+            }
+          });
+        }
+      }
+      // This function will go through each of the results, and,
+      // if the distance is LESS than the value in the picker, show it on the map.
+      function displayMarkersWithinTime(response) {
+        var maxDuration = document.getElementById('max-duration').value;
+        var origins = response.originAddresses;
+        var destinations = response.destinationAddresses;
+        // Parse through the results, and get the distance and duration of each.
+        // Because there might be  multiple origins and destinations we have a nested loop
+        // Then, make sure at least 1 result was found.
+        var atLeastOne = false;
+        for (var i = 0; i < origins.length; i++) {
+          var results = response.rows[i].elements;
+          for (var j = 0; j < results.length; j++) {
+            var element = results[j];
+            if (element.status === "OK") {
+              // The distance is returned in feet, but the TEXT is in miles. If we wanted to switch
+              // the function to show markers within a user-entered DISTANCE, we would need the
+              // value for distance, but for now we only need the text.
+              var distanceText = element.distance.text;
+              // Duration value is given in seconds so we make it MINUTES. We need both the value
+              // and the text.
+              var duration = element.duration.value / 60;
+              var durationText = element.duration.text;
+              if (duration <= maxDuration) {
+                //the origin [i] should = the markers[i]
+                markers[i].setMap(map);
+                atLeastOne = true;
+                // Create a mini infowindow to open immediately and contain the
+                // distance and duration
+                var infowindow = new google.maps.InfoWindow({
+                  content: durationText + ' away, ' + distanceText
+                });
+                infowindow.open(map, markers[i]);
+                // Put this in so that this small window closes if the user clicks
+                // the marker, when the big infowindow opens
+                markers[i].infowindow = infowindow;
+                google.maps.event.addListener(markers[i], 'click', function() {
+                  this.infowindow.close();
+                });
+              }
+            }
+          }
+        }
+        if (!atLeastOne) {
+          window.alert('We could not find any locations within that distance!');
         }
       }
